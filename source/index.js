53fc3d95515b41668d13cad4b90220e47c2fe082
Black <black_de@yahoo.com>
2023-09-26 11:33:47 +0500

    some fixes else

--- source/index.js
+++ source/index.js
@@ -7,12 +7,9 @@ async function getRepository() {
     const response = await fetch(`https://api.github.com/search/repositories?q=${inputValue}`);
     const resData = await response.json();
     clearSearchList();
-    const arrItems = resData.items.slice(0, 5)
+    const arrItems = await resData.items.slice(0, 5)
     for(let i = 0; i < arrItems.length; i++) {
-        createResponseList([resData.items[i].name,
-                            resData.items[i].owner.login,
-                            resData.items[i].stargazers_count
-                            ])
+        createResponseList([arrItems[i].name, arrItems[i].owner.login, arrItems[i].stargazers_count]);
     }
 };
 function handleTapFunction(event) {
@@ -21,7 +18,7 @@ function handleTapFunction(event) {
     inputValue = event.target.value;
     getRepository(inputValue)
 };
-function handleClickOnItem(event) {
+function handleClickOnItem(event, data) {
     const selectedElement = event
     if(event.target.tagName !== 'LI') return;
     clearSearchList();
@@ -43,20 +40,20 @@ function debounce(fn, debounceTime) {
 };
 function createResponseList(data) {
     num ++;
-    const [ name ] = data;
+    const [ name, owner, stars ] = data;
     // console.log(name, owner, stars)
     const listItem = document.createElement('li');
     listItem.className = `search-form__list-item${num}`;
     listItem.textContent = name;
     searchList.appendChild(listItem);
 }
-function createChoisenList() {
+function createChoisenList(data = ['alex', 'alexes', 1233]) {
     num ++;
-    // const [name, owner, stars] = data;
+    const [name, owner, stars] = data;
     const listItem = document.createElement('li');
     listItem.className = `result-form__list-item${num}`;
-    // listItem.textContent = data;
-    listItem.textContent = 'NAME:\n OWNER:\n STARS';
+    listItem.innerHTML = `<p>Name: ${name}</p> \n <p>Owner: ${owner}</p> \n <p>Stars: ${stars}</p>`;
+    // listItem.textContent = 'NAME:\n OWNER:\n STARS';
     resultList.appendChild(listItem);
 }
 searchInput.addEventListener('keyup', debounce(handleTapFunction, 1000))
