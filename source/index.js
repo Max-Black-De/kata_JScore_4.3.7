8d68de0e6386f7731140b23e4fecc241ea77456b
Max-Black-De <black_de@yahoo.com>
2023-09-28 00:32:53 +0500

    retry

--- source/index.js
+++ source/index.js
@@ -1,26 +1,21 @@
 const searchInput = document.querySelector('input');
 const searchList = document.querySelector('.search-form__list');
 const resultList = document.querySelector('.result-form__list');
-let num = 0;
 
-async function getRepository() {
+async function getRepository(inputValue) {
     const response = await fetch(`https://api.github.com/search/repositories?q=${inputValue}`);
     const resData = await response.json();
-    clearSearchList();
-    const arrItems = await resData.items.slice(0, 5)
-    for(let i = 0; i < arrItems.length; i++) {
-        createResponseList([arrItems[i].name, arrItems[i].owner.login, arrItems[i].stargazers_count]);
-    }
+    const arrItems = resData.items.slice(0, 5)
+    createResponseList(arrItems);
 };
+
 function handleTapFunction(event) {
     event.preventDefault();
-    console.log(event.target.value)
+    clearSearchList();
     inputValue = event.target.value;
     getRepository(inputValue)
 };
 function handleClickOnItem(event) {
-    const selectedElement = event
-    console.log(selectedElement)
     if(event.target.tagName !== 'LI') return;
     clearSearchList();
     createChoisenList()
@@ -39,25 +34,35 @@ function debounce(fn, debounceTime) {
     } 
 };
 function createResponseList(data) {
-    // num ++;
-    const [ name, owner, stars ] = data;
-    
-    const listItem = document.createElement('li');
-    listItem.className = `search-form__list-item`;
-    listItem.textContent = name;
-    // listItem.target.dataset.name = name;
-    // listItem.target.dataset.name = owner;
-    searchList.appendChild(listItem);
-    console.log(listItem.target)
-    // listItem.target.dataset.name = stars;
+    for(let i = 0; i < data.length; i++) {
+        const listItem = document.createElement('li');
+        listItem.className = `search-form__list-item`;
+        listItem.textContent = data[i].name;
+        searchList.appendChild(listItem);
+        listItem.dataset.name = data[i].name;
+        listItem.dataset.owner = data[i].owner.login;
+        listItem.dataset.stars = data[i].stargazers_count;
+    }
 }
-function createChoisenList(event) {
-    console.log(event)
-    // const [name, owner, stars] = event.target.dataset;
-    const listItem = document.createElement('li');
-    listItem.className = `result-form__list-item`;
-    // listItem.innerHTML = `<p>Name: ${name}</p> \n <p>Owner: ${owner}</p> \n <p>Stars: ${stars}</p>`;
-    resultList.appendChild(listItem);
+function createChoisenList(data) {
+    const { name, owner, stars } = data;
+    let counterLi = resultList.childNodes.length
+    if(counterLi <= 3) {
+        const listItem = document.createElement('li');
+        listItem.className = `result-form__list-item`;
+        listItem.innerHTML = `<p>Name: ${name}</p> \n <p>Owner: ${owner}</p> \n <p>Stars: ${stars}</p>`;
+        resultList.appendChild(listItem);
+    } else {
+        resultList.removeChild(resultList.childNodes[0])
+    }
+    console.log(resultList.childNodes.length);
+    console.log(resultList.childNodes[0]);
 }
-searchInput.addEventListener('keyup', debounce(handleTapFunction, 1000))
-searchList.addEventListener('click', handleClickOnItem)
+searchInput.addEventListener("keyup", debounce(handleTapFunction, 1000));
+searchList.addEventListener("click", function (event) {
+    const selectedElementData = event.target.dataset;
+    if (event.target.tagName !== "LI") return;
+    clearSearchList();
+    createChoisenList(selectedElementData);
+    searchInput.value = "";
+});
